52ba1442e696eac7a7f6ea1485fdbb57e197dccc
Damian Galeano <dj.damian.b@hotmail.com>
2023-05-08 16:46:00 +0200

    Get Request

--- app.py
+++ app.py
@@ -44,7 +44,18 @@ def add_guide():
     db.session.add(new_guide)
     db.session.commit()
 
-    
+    guide = Guide.query.get(new_guide.id)
+
+    return guide_schema.jsonify(guide)
+
+
+# Endpoint to query all guides
+
+@app.route("/guides", methods=["GET"])
+def get_guides():
+    all_guides = Guide.query.all()
+    result = guides_schema.dump(all_guides)
+    return jsonify(result)
 
 
 if __name__ == "__main__":
